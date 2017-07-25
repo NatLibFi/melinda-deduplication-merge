ec56e9be1d3d878750fb4c26384471c877de0e8d
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-07-25 13:14:45 +0300

    Adds stub implementations of dependencies

--- src/index.js
+++ src/index.js
@@ -13,8 +13,16 @@ const MelindaConnector = require('melinda-deduplication-common/utils/melinda-rec
 
 const RecordMergeService = require('melinda-deduplication-common/utils/record-merge-service');
 
-const DUPLICATE_DB_API = utils.readEnvironmentVariable('DUPLICATE_DB_API', 'http://libtest1.csc.fi:8992/API');
+const DUPLICATE_DB_API = utils.readEnvironmentVariable('DUPLICATE_DB_API');
 const MELINDA_API = utils.readEnvironmentVariable('MELINDA_API', 'http://libtest1.csc.fi:8992/API');
+const X_SERVER = utils.readEnvironmentVariable('X_SERVER', 'http://libtest1.csc.fi:8992/X');
+const MELINDA_USERNAME = utils.readEnvironmentVariable('MELINDA_USERNAME', '');
+const MELINDA_PASSWORD = utils.readEnvironmentVariable('MELINDA_PASSWORD', '');
+const MELINDA_CREDENTIALS = {
+  username: MELINDA_USERNAME,
+  password: MELINDA_PASSWORD
+};
+
 const DUPLICATE_QUEUE_AMQP_HOST = utils.readEnvironmentVariable('DUPLICATE_QUEUE_AMQP_HOST');
 
 start().catch(error => {
@@ -29,8 +37,9 @@ async function start() {
   logger.log('info', 'Connected');
 
   const duplicateDatabaseConnector = DuplicateDatabaseConnector.createDuplicateDatabaseConnector(DUPLICATE_DB_API);
-  
-  const recordMergeService = RecordMergeService.createRecordMergeService();
+  const melindaConnector = MelindaConnector.createMelindaRecordService(MELINDA_API, X_SERVER, MELINDA_CREDENTIALS);
+
+  const recordMergeService = RecordMergeService.createRecordMergeService(melindaConnector);
 
   duplicateQueueConnector.listenForDuplicates(async (duplicate, done) => {
     const pairIdentifierString = `${duplicate.first.base}/${duplicate.first.id} - ${duplicate.second.base}/${duplicate.second.id}`;
@@ -38,14 +47,15 @@ async function start() {
 
     // Load records from melinda
     logger.log('info', `Loading records from ${MELINDA_API}`);
-
+    const firstRecord = await melindaConnector.loadRecord(duplicate.first.base, duplicate.first.id);
+    const secondRecord = await melindaConnector.loadRecord(duplicate.second.base, duplicate.second.id);
 
     // check if duplicate can be merged automatically
     const automaticMergePossible = await recordMergeService.checkMergeability(firstRecord, secondRecord);
 
     if (!automaticMergePossible) {
       // CANNOT_MERGE send duplicate to duplicate db
-      await duplicateDatabaseConnector.addDuplicatePair(duplicate);
+      await duplicateDatabaseConnector.addDuplicatePair(duplicate.first, duplicate.second);
       logger.log('warn', `Duplicate pair ${pairIdentifierString} cannot be merged automatically. Pair has been sent to: ${DUPLICATE_DB_API}`);
       return done();
     }
@@ -58,10 +68,7 @@ async function start() {
 
     // mark old records as deleted
     logger.log('info', `Duplicate pair ${pairIdentifierString} has been merged to ${mergedRecordIdentifier}`);
-
-    // call done() to mark duplicate as handled.
-    
-    setTimeout(done, 10000);
+    done();
 
   });
 
